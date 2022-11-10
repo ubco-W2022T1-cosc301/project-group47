a1ae264adc2aeacbcdb61a08024c05e2c9287316
darryaa <daryabeigi92@gmail.com>
2022-11-10 13:59:55 -0800

    added new changes to my py file

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np 
 
-def load_and_process('../data/raw/world_happiness_report.csv'):
+def load_and_process(url_or_path_to_csv_file):
 
 
     df1 = (
-        pd.read_csv('../data/raw/world_happiness_report.csv'))
+        pd.read_csv(url_or_path_to_csv_file))
 
     df2 = ( df1.transform(pd.to_numeric, errors='coerce')
          .copy().drop(['Happiness Rank', 'Country', 'Region', 'Freedom', 'Trust (Government Corruption)', 'Generosity'], axis=1)
          .copy().drop(df1.columns[0], axis=1)
          .dropna(axis=0))
     
-    df3 = (df1[df1.Year != 2022])
+    df3 = (df2[df2.Year != 2022])
     
     # Make sure to return the latest dataframe
     return df3
