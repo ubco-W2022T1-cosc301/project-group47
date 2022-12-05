c2cf381d715bd69482ce0e958411aca93fbde9d5
darryaa <daryabeigi92@gmail.com>
2022-12-04 16:38:46 -0800

    fixed my graph

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -1,5 +1,12 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
+
+def normalize1(df):
+    if df.name!="Year":
+        max_value = df.max()
+        min_value = df.min()
+        df = df.apply(lambda x: (x- min_value) / (max_value - min_value))
+    return df
 
 def load_and_process(url_or_path_to_csv_file):
 
@@ -12,14 +19,9 @@ def load_and_process(url_or_path_to_csv_file):
          .drop(df1.columns[0], axis=1)
          .dropna(axis=0)
          [df1["Year"]!=2022]
-           .apply(normalize1)
+         .apply(normalize1)
           )
-    
-    df3 = normalize1(df2)
-    
-    
- 
-    
+
     # Make sure to return the latest dataframe
-    return df3
+    return df2
     
