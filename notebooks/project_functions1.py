cb460417b6ca74a23f909ea9e562ee2cfafcc253
annaclaradeandrada <andradaa@student.ubc.ca>
2022-12-05 17:10:54 -0800

    added method chain to py file

--- notebooks/project_functions1.py
+++ notebooks/project_functions1.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import numpy as np 
 
+
+
 def load_and_process(url_or_path_to_csv_file):
 
+    convert_dict = {'Happiness Score': float,
+                'Economy (GDP per Capita)': float, 
+               'Family (Social Support)': float,
+               'Health (Life Expectancy)':float,
+               'Freedom':float, 
+               'Country':str, 
+               'Region':str, 
+               'Year':float, 
+               'Happiness Rank':float, 
+               'Trust (Government Corruption)':float,
+                'Generosity':float
+               }
 
     dfa = (
         pd.read_csv(url_or_path_to_csv_file))
-
-    dfb = ( dfa.transform(pd.to_numeric, errors='coerce')
-         .drop(columns=['Country', 'Region']))
-    dfc = dfb[dfb.Freedom.notnull()]
-    dfd = dfc.drop(columns=dfc.columns[0])
+    dfb = (dfa[dfa.Year != 2022]
+            .drop(dfa.columns[0], axis=1)
+              .apply(convert_dict))
     
-    # Make sure to return the latest dataframe
-    return dfd
+    return dfb
