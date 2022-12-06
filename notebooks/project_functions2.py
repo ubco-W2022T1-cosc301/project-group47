7b5d0de5f2ec0809cd2950a77f1b7c6b8a5f6e08
Yahia Kamel Hussein <yhussein@student.ubc.ca>
2022-12-05 16:31:25 -0800

    fixed some of my method chaining

--- notebooks/project_functions2.py
+++ notebooks/project_functions2.py
@@ -3,21 +3,17 @@ import numpy as np
 
 def load_and_process(url_or_path_to_csv_file):
     
-    convert_dict = {'Happiness Score': float,
-                'Economy (GDP per Capita)': float, 
-               'Family (Social Support)': float,
-               'Health (Life Expectancy)':float,
-               'Freedom':float, 
-               'Country':str, 
-               'Region':str, 
-               'Year':float, 
-               'Happiness Rank':float, 
-               'Trust (Government Corruption)':float,
-                'Generosity':float
-               }
-    
     def normalize_with_percent(df):
+        
+        df["Happiness Score"]=df["Happiness Score"].astype(float)
+        df["Economy (GDP per Capita)"]=df["Economy (GDP per Capita)"].astype(float)
+        df["Family (Social Support)"]=df["Family (Social Support)"].astype(float)
+        df["Health (Life Expectancy)"]=df["Health (Life Expectancy)"].astype(float)
+        df["Freedom"]=df["Freedom"].astype(float)
+        df["Trust (Government Corruption)"]=df["Trust (Government Corruption)"].astype(float)
+        
         result = df.copy()
+        
         for feature_name in df.columns:
             if df[feature_name].dtype==float:
                 # if dtype is float then normalize it
@@ -29,25 +25,14 @@ def load_and_process(url_or_path_to_csv_file):
     dfa = (
         pd.read_csv(url_or_path_to_csv_file))
     dfb = (dfa[dfa.Year != 2022]
-            .drop(dfa.columns[0], axis=1)
-              .apply(convert_dict))
+          .drop(columns = ['Unnamed: 0', 'Year', 'Generosity'], axis=1))
 
+    dfc = (normalize_with_percent(dfb).groupby(['Country'], as_index = False, sort = False)['Happiness Score', 'Economy (GDP per Capita)',
+                                           'Family (Social Support)', 'Health (Life Expectancy)', 
+                                           'Freedom', 'Trust (Government Corruption)'].mean())
     
-#     df = (pd.read_csv(url_or_path_to_csv_file))
-#     df_cleaned = (df[df.Region != '-'])
-#     df_cleaned_2 = (df_cleaned.copy().drop(['Generosity'], axis=1)
-#                      .copy().drop(df_cleaned.columns[0], axis=1)
-#                      .dropna(axis=0))
-#     df_cleaned_3 = (df_cleaned_2[df_cleaned_2.Year != 2022])
-#     df_cleaned_4 = (df_cleaned_3.copy().drop(['Year'], axis=1)
-                    
-#     df_cleaned_4["Happiness Score"]=df_cleaned_4["Happiness Score"].astype(float)
-#     df_cleaned_4["Economy (GDP per Capita)"]=df_cleaned_4["Economy (GDP per Capita)"].astype(float)
-#     df_cleaned_4["Family (Social Support)"]=df_cleaned_4["Family (Social Support)"].astype(float)
-#     df_cleaned_4["Health (Life Expectancy)"]=df_cleaned_4["Health (Life Expectancy)"].astype(float)
-#     df_cleaned_4["Freedom"]=df_cleaned_4["Freedom"].astype(float)
-#     df_cleaned_4["Trust (Government Corruption)"]=df_cleaned_4["Trust (Government Corruption)"].astype(float)
+
 
     # Make sure to return the latest dataframe
-    return dfb
+    return dfc
     
